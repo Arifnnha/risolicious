5efc8596e5f3988c38503e2b5d282e6d6006aabc
Arif <muhamadarifulinnuha@gmail.com>
2024-11-14 11:29:12 +0700

    Initial commit Risol Mayo

---
+++ components/navbar.php
@@ -0,0 +1,39 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light px-3">
+    <!-- Brand -->
+    <a class="navbar-brand" href="index.php">
+        <i class="bi bi-basket-fill"></i> Risol Mayo
+    </a>
+    <!-- Toggler/collapsible Button for Mobile -->
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <!-- Navbar Links -->
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+            <!-- Daftar Menu -->
+            <li class="nav-item">
+                <a class="nav-link" href="menu.php">
+                    <i class="bi bi-card-list"></i> Daftar Menu
+                </a>
+            </li>
+            <!-- Keranjang -->
+            <li class="nav-item">
+                <a class="nav-link" href="cart.php">
+                    <i class="bi bi-cart"></i> Keranjang
+                </a>
+            </li>
+            <!-- Daftar -->
+            <li class="nav-item">
+                <a class="nav-link" href="register.php">
+                    <i class="bi bi-person-plus"></i> Daftar
+                </a>
+            </li>
+            <!-- Login -->
+            <li class="nav-item">
+                <a class="nav-link" href="login.php">
+                    <i class="bi bi-box-arrow-in-right"></i> Login
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
